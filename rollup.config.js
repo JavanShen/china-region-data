0645e08bb943b8bf1a663549734cce099c3096ac
apine <1985486089@qq.com>
2022-09-28 11:24:42 +0800

    :tada: 初始化

---
+++ rollup.config.js
@@ -0,0 +1,14 @@
+import json from "rollup-plugin-json";
+import typescript from "@rollup/plugin-typescript";
+import pkg from "./package.json";
+export default {
+  input: "src/main.ts",
+  output: [
+    {
+      name: pkg.name,
+      file: pkg.main,
+      format: "umd",
+    },
+  ],
+  plugins: [json(), typescript({ lib: ["es5", "es6", "dom"], target: "es5" })],
+};

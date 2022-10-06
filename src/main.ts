af2d2dcb210d6282c3e17b9bc0613082df11c5bb
apine <1985486089@qq.com>
2022-10-06 19:22:50 +0800

    :hammer: 入口文件导出类型

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
 export { default as province } from "@/region/province";
 export { default as city } from "@/region/city";
 export { default as county } from "@/region/county";
+
+export * from '@/types/regions'
